a26802f2688cd8be5a6281a2f423fd34688cec5a
bangdc <bang.dc@outlook.com>
2016-02-04 23:51:32 +0100

    Fake seed database

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Maker;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -12,9 +14,14 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+		//Truncate statement of MySQL
+		Maker::truncate();
 		Model::unguard();
 
 		// $this->call('UserTableSeeder');
+		$this->call('MakerSeed');
+		$this->call('VehicleSeed');
 	}
 
 }
